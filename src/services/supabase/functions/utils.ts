85164d5ff184f40cc7976ac4928437996a51b292
KikeDeAlba <likingvirus1@gmail.com>
2024-05-15 09:39:27 -0700

    Refactor userHasPermissionToEnter function to use userType constants for URL checks

--- src/services/supabase/functions/utils.ts
+++ src/services/supabase/functions/utils.ts
@@ -12,11 +12,11 @@ export const foundUserRedirect = (userType: USER_TYPES) => urls[userType] ?? '/n
 export const userHasPermissionToEnter = (userType: USER_TYPES, pathname: string) => {
   if (userType === USER_TYPES.ADMIN) return true
 
-  if (userType === USER_TYPES.PROFESSOR && pathname.startsWith(urls[2])) return true
+  if (userType === USER_TYPES.PROFESSOR && pathname.startsWith(urls[USER_TYPES.PROFESSOR])) return true
 
-  if (userType === USER_TYPES.STUDENT && pathname.startsWith(urls[3])) return true
+  if (userType === USER_TYPES.STUDENT && pathname.startsWith(urls[USER_TYPES.STUDENT])) return true
 
-  if (userType === USER_TYPES.COORDINATOR && pathname.startsWith(urls[4])) return true
+  if (userType === USER_TYPES.COORDINATOR && pathname.startsWith(urls[USER_TYPES.COORDINATOR])) return true
 
   return false
 }
