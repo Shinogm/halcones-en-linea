459e2dfa7d4fed47304cf34fd231ca876a490418
KikeDeAlba <likingvirus1@gmail.com>
2024-05-20 14:40:04 -0700

    refactor types for add coordinator to career

--- src/services/supabase/actions/coordinators.ts
+++ src/services/supabase/actions/coordinators.ts
@@ -17,36 +17,12 @@ export const getAllCoordinators = async () => {
 export const getCoordinatorCareers = async (id: string) => {
   const supabase = await createClient()
 
-  const { data, error } = await supabase.from('coordinator_careers').select('careers(id, name)').eq('coordinator', id)
+  const { data: careers, error } = await supabase.from('careers').select('id name').eq('coordinator', id)
 
   if (error != null) {
     console.error('Error getting coordinator careers:', error)
     throw new Error('Error getting coordinator careers')
   }
 
-  const careers = data.map((career) => career.careers).filter((career) => career != null)
-
   return careers
-}
-
-export const addCareerToCoordinator = async (coordinator: string, career: number) => {
-  const supabase = await createClient()
-
-  const { error } = await supabase.from('coordinator_careers').insert({ coordinator, career })
-
-  if (error != null) {
-    console.error('Error adding career to coordinator:', error)
-    throw new Error('Error adding career to coordinator')
-  }
-}
-
-export const removeCareerFromCoordinator = async (coordinator: string, career: number) => {
-  const supabase = await createClient()
-
-  const { error } = await supabase.from('coordinator_careers').delete().eq('coordinator', coordinator).eq('career', career)
-
-  if (error != null) {
-    console.error('Error removing career from coordinator:', error)
-    throw new Error('Error removing career from coordinator')
-  }
 }
